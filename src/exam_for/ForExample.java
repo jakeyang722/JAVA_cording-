46070c4a6f86027d4298d802585fe447ee94370f
jakeyang722 <wnsdud722@gmail.com>
2026-05-08 13:38:45 +0900

    test2

--- src/exam_for/ForExample.java
+++ src/exam_for/ForExample.java
@@ -15,6 +15,8 @@ public class ForExample {
         System.out.println("3의 배수를 1~10까지 더한 값: " + sum);
         System.out.println("3의 배수를 1~10까지 더한 값: " + sum);
 
+        System.out.println("3의 배수를 1~10까지 더한 값: " + sum);
+
 
     }
 }
